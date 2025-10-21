1bd4ecd26ba4228406630afbaaac614c27031936
Jared Dube <jdube3@academic.rrc.ca>
2025-10-20 23:54:38 -0500

    Updated app.ts to use various loggers depending on runtime environment, error handling middleware and updated the health check to be a little more robust and have a defined shape

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,44 @@
 import express, { Express } from "express";
 import morgan from "morgan";
+import loanRoutes from "./api/v1/routes/loanRoutes";
+import { 
+    accessLogger, 
+    errorLogger, 
+    consoleLogger } from "./api/v1/middleware/logger";
+import errorHandler from "./api/v1/middleware/errorHandler";
 
 const app: Express = express();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(accessLogger);
+    app.use(errorLogger);
+} else {
+    app.use(consoleLogger);
+}
+
+interface HealthCheckResponse {
+    status: string;
+    uptime: number;
+    timestamp: string;
+    version: string;
+}
 
 app.use (express.json());
 app.use(morgan("combined"));
 
 app.get("/api/v1/health", (req: express.Request, res: express.Response) => {
-    res.json({
+    const healthData: HealthCheckResponse = {
         status: "OK",
         uptime: process.uptime(),
         timestamp: new Date().toISOString(),
         version: "1.0.0",
-    });
+    };
+
+    res.json(healthData);
 });
+
+app.use("api/v1/loans", loanRoutes);
+
+app.use(errorHandler);
 
 export default app;
